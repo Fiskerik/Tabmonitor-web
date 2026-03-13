e14bce7ee12be0809db1d679841f8f853fedd9f4
Fiskerik <63838726+Fiskerik@users.noreply.github.com>
2026-03-13 09:56:13 +0100

    Create page.tsx

---
+++ app/compare/page.tsx
@@ -0,0 +1,73 @@
+export default function ComparePage() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Feature</th>
+          <th>Tab Monitor</th>
+          <th>OneTab</th>
+          <th>The Great Suspender</th>
+          <th>Workona</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Focus Mode Lock</td>
+          <td>✅</td>
+          <td>❌</td>
+          <td>❌</td>
+          <td>❌</td>
+        </tr>
+        <tr>
+          <td>Real-time RAM monitoring</td>
+          <td>✅</td>
+          <td>❌</td>
+          <td>⚠️ Estimates</td>
+          <td>❌</td>
+        </tr>
+        <tr>
+          <td>Advanced usage analytics</td>
+          <td>✅</td>
+          <td>❌</td>
+          <td>❌</td>
+          <td>❌</td>
+        </tr>
+        <tr>
+          <td>Multi launguage support</td>
+          <td>✅</td>
+          <td>❌</td>
+          <td>❌</td>
+          <td>❌</td>
+        </tr>
+        <tr>
+          <td>Backed-up sessions</td>
+          <td>✅</td>
+          <td>❌</td>
+          <td>❌</td>
+          <td>✅</td>
+        </tr>
+                <tr>
+          <td>Sorting / Quick-actions</td>
+          <td>✅</td>
+          <td>✅</td>
+          <td>❌ </td>
+          <td>✅</td>
+        </tr>
+        <tr>
+          <td>Suspending of tabs</td>
+          <td>✅</td>
+          <td>✅</td>
+          <td>✅ </td>
+          <td>❌</td>
+        </tr>
+                                <tr>
+          <td>Tab groups</td>
+          <td>✅</td>
+          <td>✅</td>
+          <td>✅</td>
+          <td>✅</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
